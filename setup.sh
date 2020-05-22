fb7bc49e4c6ad7ba9e8f97ce20410000f4e6d0d7
Your Name <you@example.com>
2020-05-21 20:09:14 -0500

    fix setup config

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p datadir/ datadir/mosquitto/ datadir/grafana/
-sudo chown -R 1883 datadir/mosquitto/
-sudo chown -R 472 datadir/grafana/
+sudo chown -R 1883:root datadir/mosquitto/
+sudo chown -R 472:root datadir/grafana/
