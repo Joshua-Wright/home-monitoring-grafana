40419823d01448db9b81381f90e8b5aaadc221a7
Your Name <you@example.com>
2020-05-24 15:09:27 -0500

    add bridge for my air quality sensor

---
+++ temtop-elitech-cloud-bridge/main.py
@@ -0,0 +1,69 @@
+# main.py
+import os
+from influxdb import InfluxDBClient
+from temtop_api import TemtopApi
+from datetime import datetime, timedelta
+import pytz
+from time import sleep
+
+TEMPTOP_USER = os.environ.get('TEMPTOP_USER')
+TEMPTOP_PASS = os.environ.get('TEMPTOP_PASS')
+
+INFLUXDB_ADDRESS  = os.environ.get('INFLUXDB_ADDRESS')
+INFLUXDB_USER     = os.environ.get('INFLUXDB_USER')
+INFLUXDB_PASSWORD = os.environ.get('INFLUXDB_PASSWORD')
+INFLUXDB_DATABASE = os.environ.get('INFLUXDB_DATABASE')
+
+influxdb_client = InfluxDBClient(INFLUXDB_ADDRESS, 8086, INFLUXDB_USER, INFLUXDB_PASSWORD, None)
+
+
+
+def _init_influxdb_database():
+    databases = influxdb_client.get_list_database()
+    if len(list(filter(lambda x: x['name'] == INFLUXDB_DATABASE, databases))) == 0:
+        influxdb_client.create_database(INFLUXDB_DATABASE)
+    influxdb_client.switch_database(INFLUXDB_DATABASE)
+
+
+def main():
+    _init_influxdb_database()
+
+    while True:
+
+        temtopApi = TemtopApi(TEMPTOP_USER, TEMPTOP_PASS)
+
+        deviceId = temtopApi.getFirstDeviceId()
+        
+        endDateTime = datetime.now()
+        startDateTime = endDateTime - timedelta(minutes=30)
+        # startDateTime = endDateTime - timedelta(days=30)
+        # end 5 minutes in the future, just in case there is some time skew
+        endDateTime += timedelta(minutes=5)
+        deviceData = temtopApi.getM10iDeviceData(
+            deviceId=deviceId,
+            startDateTime=startDateTime,
+            endDateTime=endDateTime,
+        )
+
+        points = []
+        for d in deviceData:
+            timestamp = d['datetime']
+            for reading_name in temtopApi.readings:
+                reading = d[reading_name]
+                point = {
+                    'time': timestamp.astimezone(pytz.utc),
+                    'measurement': reading_name,
+                    'tags': { 'location': 'M10i' },
+                    'fields': { 'value': reading },
+                }
+                print(point)
+                points.append(point)
+        print(f'writing {len(points)} to influxdb')
+        success = influxdb_client.write_points(points)
+        print(success)
+
+        sleep(10*60)
+
+if __name__ == '__main__':
+    print('temptop M10i (elitech cloud) to InfluxDB bridge')
+    main()
---
+++ temtop-elitech-cloud-bridge/temtop_api.py
@@ -0,0 +1,113 @@
+# download_data.py
+import requests
+from datetime import datetime, timedelta
+import pytz
+
+
+class TemtopApi:
+    readings = [ 'HCHO', 'PM2.5', 'TVOC', 'AQI' ]
+
+    def __init__(self, username, password):
+        # do login
+        self.headers = {
+            'User-Agent': 'okhttp/2.7.5',
+        }
+        r = requests.get(
+            f'http://www.i-elitech.com//apiLoginAction.do?method=login&password={password}&username={username}',
+            headers=self.headers,
+        )
+        resp = r.json()
+        if not resp['success']:
+            raise RuntimeError('login failed')
+        self.userId = resp['user']['id']
+        self.token = resp['token']
+        self.timezone = pytz.timezone("America/Chicago")
+
+
+    def get(self, url: str, params: None):
+        headers = {
+            'User-Agent': 'okhttp/2.7.5',
+            'JSESSIONID': self.token,
+        }
+        cookies = { 'JSESSIONID': self.token }
+        r = requests.get(url, headers=headers, cookies=cookies, params=params)
+        resp = r.json()
+        if not resp['success']:
+            print(f'request failed, response={resp}')
+            raise RuntimeError('request failed')
+        return resp
+
+    def getDeviceList(self):
+        params={
+            'method':   'getList',
+            'typeList': '0',
+            'userId':   self.userId,
+        }
+        return self.get('http://www.i-elitech.com//apiDeviceAction.do', params=params)['rows']
+    
+    def getFirstDeviceId(self) -> int:
+        return self.getDeviceList()[0]['id']
+
+    def getDeviceData(self, deviceId: int, startDateTime: datetime, endDateTime: datetime):
+        params = {
+            'method'    : 'getList',
+            'page'      : '1',
+            'rows'      : '4500',
+            'startDate' : startDateTime.isoformat(sep=' ', timespec='seconds'),
+            'endDate'   : endDateTime.isoformat(sep=' ', timespec='seconds'),
+            'deviceId'  : deviceId,
+        }
+        return self.get('http://www.i-elitech.com//apiDeviceDataAction.do', params=params)['rows']
+    
+    def getM10iDeviceData(self, deviceId: int, startDateTime: datetime, endDateTime: datetime):
+        data = self.getDeviceData(deviceId, startDateTime, endDateTime)
+        
+        def parseDate(createTime) -> datetime:
+            return datetime(
+                day     = createTime['date'],
+                hour    = createTime['hours'],
+                minute  = createTime['minutes'],
+                month   = createTime['month']+1,
+                second  = createTime['seconds'],
+                year    = createTime['year']+1900,
+                tzinfo  = self.timezone,
+            )
+        return [
+            {
+                # 'originaldatetime': d['createTime'],
+                'datetime' : parseDate(d['createTime']),
+                'HCHO'     : float(d['probe1']),
+                'PM2.5'    : float(d['probe2']),
+                'TVOC'     : float(d['probe3']),
+                'AQI'      : float(d['probe4']),
+            }
+            for d in data
+        ]
+
+
+if __name__ == '__main__':
+    import os
+
+    username = os.environ.get('TEMPTOP_USER')
+    password = os.environ.get('TEMPTOP_PASS')
+    api = TemtopApi(username, password)
+
+    deviceId = api.getFirstDeviceId()
+    print(f'device id: {deviceId}')
+
+    now = datetime.now()
+    midnight = now.replace(hour=0, minute=0, second=0)
+    # end 5 minutes in the future, just in case there is some time skew
+    now += timedelta(minutes=5)
+    deviceData = api.getM10iDeviceData(
+        deviceId=deviceId,
+        startDateTime=midnight,
+        endDateTime=now,
+    )
+    print('data for today:')
+    for d in deviceData:
+        print(d)
+
+    for reading in [ 'HCHO', 'PM2.5', 'TVOC', 'AQI' ]:
+        print(f'min {reading}: {min(deviceData, key=lambda x: x[reading])}')
+
